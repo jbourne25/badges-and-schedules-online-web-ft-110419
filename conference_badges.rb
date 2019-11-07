a7fb7f49959aa7b6dbca1ea48bab7180aea6390f
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-07 18:51:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+#speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", #"Linus", "Matz"]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
