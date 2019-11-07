0aa448f5e7cb31b595e2fc516630a86ec3fab180
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-07 19:59:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ end
 
 def assign_rooms(speakers)
   counter = 1
-  speakers.each_index do |name|
+  speakers.each_with_index do |name|
     return "Hello,#{name}! You'll be assigned to room #{counter}"
     counter += l
   end
