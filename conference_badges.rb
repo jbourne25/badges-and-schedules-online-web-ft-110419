e2301401597d10ae1bc330c1975aa97b7e1eb1fc
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-07 20:29:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,17 +11,7 @@ badge_maker(name)
   end
 end 
  
-def assign_rooms(array)
-  nuarray = []
-  counter = 1
-   array.each do |name|
-    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!)
-    counter += 1
-  end
-  return nuarray
-end
 
-assign_rooms(speakers)
 
 
 
