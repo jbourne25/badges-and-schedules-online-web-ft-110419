2682e3fbf0f7fd9d10e09c05a7d5cf1584c27ed6
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-07 18:38:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(speaker)
   puts "Hello, my name is #{speaker}."
 end 
 
-batch_badge_creator(name)
+batch_badge_creator(conference_speakers)
 conference_speakers.collect do |name|
   badge_maker(speaker)
 end 
