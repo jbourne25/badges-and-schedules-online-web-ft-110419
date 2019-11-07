f86116b6e20ce2c95167003476286d8fa92b91ce
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-07 19:29:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,14 @@ end
 #   return nuarray
 # end 
 
-
+def batch_badge_creator(speakers)
+  # returns an array of badge messages, using badge_maker
+  badge_messages = []
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
+  end
+  badge_message
     
 
 
