5d0f87d21286660dd674b171c4233702ead59231
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-07 20:16:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,23 +11,23 @@ badge_maker(name)
   end
 end 
  
-# def batch_badge_creator(array)
-#   nuarray = []
-#   array.each do |name|
-#     nuarray.push("Hello, my name is #{name}.")
+ def batch_badge_creator(array)
+   nuarray = []
+   array.each do |name|
+     nuarray.push("Hello, my name is #{name}.")
+   end
+   return nuarray
+ end 
+
+# def assign_rooms(speakers)
+#   counter = 1
+#   speakers.each_with_index do |name|
+#     return "Hello, #{name}! You'll be assigned to room #{counter}"
+#     counter += l
 #   end
-#   return nuarray
 # end 
 
-def assign_rooms(speakers)
-  counter = 1
-   speakers.each_with_index do |name|
-    return "Hello, #{name}! You'll be assigned to room #{counter}"
-    counter += l
-  end
-end 
-
-assign_rooms(speakers)  
+# assign_rooms(speakers)  
 
 
 
