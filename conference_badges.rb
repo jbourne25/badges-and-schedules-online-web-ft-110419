9203585f44acaf7b07002c624e5b6b219e8b59bb
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-07 19:24:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,20 +3,20 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
-
+badge_maker("Arel")
 #batch_badge_creator(speakers)
  # speakers.collect do |name|
 #badge_maker(name)
 #  end
 #end 
  
-def batch_badge_creator(array)
-  nuarray = []
-  array.each do |name|
-    nuarray.push("Hello, my name is #{name}.")
-  end
-  return nuarray
-end  
+# def batch_badge_creator(array)
+#   nuarray = []
+#   array.each do |name|
+#     nuarray.push("Hello, my name is #{name}.")
+#   end
+#   return nuarray
+# end  
     
 
 
